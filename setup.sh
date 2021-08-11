9bd415e432bf2dc29bcaac9b601b44aa86d02787
s-kato <s-kato@ar.sanken.osaka-u.ac.jp>
2021-08-11 20:56:43 +0900

    環境設定

--- setup.sh
+++ setup.sh
@@ -6,4 +6,8 @@ conda install -c conda-forge nodejs
 # table of contents
 jupyter labextension install @jupyterlab/toc
 
-# 
+# enable widgets in jupyterlab
+jupyter labextension install @jupyter-widgets/jupyterlab-manager
+
+# variable inspector
+jupyter labextension install @lckr/jupyterlab_variableinspector
